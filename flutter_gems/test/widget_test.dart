0189415052499d22b62c2439bc3e3cdf2c6e9859
adnanullah <daaztanbd71@gmail.com>
2026-03-11 12:57:39 +0600

    Remove trailing whitespace in `AppError` class
    - Remove unnecessary whitespace in `packages/gems_core/lib/src/result/app_error.dart`.

--- flutter_gems/test/widget_test.dart
+++ flutter_gems/test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_gems/main.dart';
+import '../lib/main.dart';
 
 void main() {
   testWidgets('FlutterGemsApp renders welcome message', (WidgetTester tester) async {
