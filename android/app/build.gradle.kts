f23d405c7fac143a1c9070b90ef6b1b575e2cafe
adnanullah <daaztanbd71@gmail.com>
2026-03-25 12:42:37 +0600

    Configure Android release signing and update app versioning
    - Implement logic to load signing credentials from `key.properties` in `build.gradle.kts`.
    - Define a `release` signing configuration using the loaded keystore properties.
    - Update the `release` build type to use the new signing configuration and disable minification and resource shrinking.
    - Increment `versionCode` to `2` and update `versionName` to `1.0.2`.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,12 @@
+import java.util.Properties
+import java.io.FileInputStream
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -19,6 +28,15 @@ android {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "net.encoderit.encoderit_groceries"
@@ -26,15 +44,15 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode =   2//flutter.versionCode
+        versionName = "1.0.2"//flutter.versionName
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
