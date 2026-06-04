a1fbe40ba10f26cc2e932adbbaf5c43455917f4b
adnanullah <daaztanbd71@gmail.com>
2026-06-04 10:27:21 +0600

    Update WooCommerce API credentials in `woo_auth_config.dart`
    - Update `consumerKey` and `consumerSecret` with new static credentials.

--- lib/utils/woo_auth_config.dart
+++ lib/utils/woo_auth_config.dart
@@ -2,9 +2,9 @@ class WooAuthConfig {
   /// WooCommerce API credentials. For now fixed; after auth is ready
   /// these will be set dynamically from the login API response.
   static String consumerKey =
-      'ck_0cdadeb2f0389f090bcb897fb0f52109d3b8f8b7';
+      'ck_1cd2d74318be5bf0775a0896c8b3b3f4e2e2cedb';
   static String consumerSecret =
-      'cs_7f766c1eacf557d0a230328a2ead7e354e2ef83d';
+      'cs_abcbf70b6248116ff2ac40cf2fb12a2eeaefaf19';
 
   static String get authQuery =>
       'consumer_key=$consumerKey&consumer_secret=$consumerSecret';
