aeb75ecb2508cb439988cb160df56bc1e00dc6c1
adnanullah <daaztanbd71@gmail.com>
2026-06-11 13:56:57 +0600

    Update project version and synchronize Android build configuration
    - Bump project version from `1.0.0+1` to `1.0.4+4` in `pubspec.yaml`.
    - Revert hardcoded `versionCode` and `versionName` in `android/app/build.gradle.kts` to use Flutter's default environment variables.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode =   3//flutter.versionCode
-        versionName = "1.0.3"//flutter.versionName
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
